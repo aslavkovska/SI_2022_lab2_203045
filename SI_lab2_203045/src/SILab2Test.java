a7f866729ae8b5d0812bac4bbd4ccc0e78c00be2
aslavkovska <anastasija.slavkovska@yahoo.com>
2022-06-17 01:47:31 +0200

    Add SI_lab2_203045/src/SILab2Test.java

--- SI_lab2_203045/src/SILab2Test.java
+++ SI_lab2_203045/src/SILab2Test.java
@@ -1,4 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SILab2Test {
-  
+
+    private final SILab2 silab2 = new SILab2();
+
+    @Test
+    void standardAssertions() {
+        assert equals( "[#, 3, #, 2, #, 2, 0, 1, 0]" , silab2.function("[#, 0, #, 0, #, 0, 0, 0, 0]"));
+    }
 }
